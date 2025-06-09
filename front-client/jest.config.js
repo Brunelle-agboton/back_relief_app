15e41b489984aefa6f5b9ebc9b34e8e075dcf291
Brunelle-agboton <77045545+Brunelle-agboton@users.noreply.github.com>
2025-06-09 02:16:24 +0200

    Update and rename jest.config.js to delet2

--- front-client/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|expo(nent)?|expo-router)/)',
-  ],
-  testPathIgnorePatterns: ['/node_modules/', '/build/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-};
