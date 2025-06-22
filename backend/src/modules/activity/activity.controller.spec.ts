6437933fb1ed146c0ce04b5bdad91305b3b78458
agboto_b <agboto_b@etna-alternance.net>
2025-06-22 21:27:26 +0200

    Back tests config

--- backend/src/modules/activity/activity.controller.spec.ts
+++ backend/src/modules/activity/activity.controller.spec.ts
@@ -1,14 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import {ActivityController } from './activity.controller';
 import {ActivityService } from './activity.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Activity } from './entities/activity.entity';
+import { UserService } from '../user/user.service';
 
 describe('ActivityController', () => {
   let controller:ActivityController;
+  let activityService: Partial<Record<keyof ActivityService, jest.Mock>>;
+  let userService: Partial<Record<keyof UserService, jest.Mock>>;
+
+  const mockRepo = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    // etc.
+  };
+
+  userService = {
+      findOne: jest.fn(),
+    };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActivityController],
-      providers: [ActivityService],
+      providers: [ActivityService, 
+        {provide: getRepositoryToken(Activity),useValue: mockRepo,
+        },
+        { provide: UserService, useValue: userService },
+],
     }).compile();
 
     controller = module.get<ActivityController>(ActivityController);
--- backend/src/modules/activity/activity.service.spec.ts
+++ backend/src/modules/activity/activity.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PainService } from './activity.service';
+import { ActivityService } from './activity.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Activity } from './entities/activity.entity';
 
-describe('PainService', () => {
-  let service: PainService;
-
+describe('ActivityService', () => {
+  let service: ActivityService;
+  
+  const mockRepo = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    // etc.
+  };
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PainService],
+      providers: [ActivityService,
+         {provide: getRepositoryToken(Activity),useValue: mockRepo},
+      ],
     }).compile();
 
-    service = module.get<PainService>(PainService);
+    service = module.get<ActivityService>(ActivityService);
   });
 
   it('should be defined', () => {
